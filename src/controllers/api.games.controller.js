d0849c6b635a4a3a64bc05960c38cacb695ddfaa
Orhun Özer <orhunozor@gmail.com>
2019-02-23 00:12:43 +0300

    Develop (#2)
    * .DS_Store banished!
    
    * Update gitignore
    
    * Fix games endpoint

--- src/controllers/api.games.controller.js
+++ src/controllers/api.games.controller.js
@@ -3,7 +3,7 @@ const https = require('https');
 // GET games
 const getGames = async (req, res) => {
   try {
-    await https.get('https://www.balldontlie.io/api/v1/teams', (resp) => {
+    await https.get('https://www.balldontlie.io/api/v1/games', (resp) => {
       let data = '';
 
       resp.on('data', (chunk) => {
